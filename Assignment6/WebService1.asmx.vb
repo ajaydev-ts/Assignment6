5941987771e487c82c83ffec1038249d1dd88751
ajaydev-ts <ajaydev.ts@gadgeon.com>
2022-12-08 16:57:46 +0530

    first commit

---
+++ Assignment6/WebService1.asmx.vb
@@ -0,0 +1,27 @@
+﻿Imports System.ComponentModel
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+Imports System.Data.SqlClient
+Imports System.Data
+Imports System.Configuration
+
+
+' To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
+' <System.Web.Script.Services.ScriptService()> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
+<ToolboxItem(False)> _
+Public Class WebService1
+    Inherits System.Web.Services.WebService
+
+    [WebMethod]
+    Public DataSet GetEmployeeByID(int EmployeeId)
+    {
+      DataSet ds = New DataSet();
+        
+       Return ds;
+    }
+
+    
+
+End Class
